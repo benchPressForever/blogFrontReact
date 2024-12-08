86ed519420dd1af1a9c548781baf92908ac7eb80
killu456 <131191776+killu456@users.noreply.github.com>
2024-12-08 20:59:11 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Element } from "react-scroll";
 import { AppRouter } from "./components/AppRouter";
 import { Header } from "./components/HeaderC/Header";
-import { LegalA } from "./pages/LegalAssistance/LegalA";
+
 import { MyInfo } from "./pages/MyInfoPage/MyInfo";
 import { Services } from "./pages/ServicesPage/Services";
 import { CONSULTATION_ROUTE, CONTACTS_ROUTE, MAIN_ROUTE, MYINFO_ROUTE, SERVICES_ROUTE } from "./utils/constants";
